8c864307da8008c69d2ba5275744d0cadbaac353
c0a242346c <c0a242346c@edu.teu.ac.jp>
2025-04-15 15:51:51 +0900

    こうかとん、移動する

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -17,8 +17,8 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        x = tmr
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
