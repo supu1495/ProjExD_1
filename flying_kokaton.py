c3838222f9d706baac079bb2a7e9e1802bb78b8f
c0a242346c <c0a242346c@edu.teu.ac.jp>
2025-04-15 16:03:24 +0900

    背景の雲を連結

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
@@ -19,7 +20,7 @@ def main():
             if event.type == pg.QUIT: return
         x = tmr
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x+1600, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
