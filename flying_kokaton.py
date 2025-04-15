88f8b41aa2e71609efa5f4065f4ae8570b59b6c3
c0a242346c <c0a242346c@edu.teu.ac.jp>
2025-04-15 17:52:17 +0900

    演習問題にした

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -21,22 +21,28 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
-        #x = tmr%3200
+
+        up = key_lst[pg.K_UP]
+        down = key_lst[pg.K_DOWN]
+        right = key_lst[pg.K_RIGHT]
+        left = key_lst[pg.K_LEFT]
+
+        dx= right-left
+        dy= down-up
+
+        if right and not left:
+            dx += 1
+        else:
+            dx -= 1
+
+
+        kk_rct.move_ip((dx, dy))
         x = tmr%1600
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
-        #screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, kk_rct)
         pg.display.update()
-        tmr += 1        
+        tmr += 1      
         clock.tick(200)
 
 
