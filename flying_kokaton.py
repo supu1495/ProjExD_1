d7d7ebd2b0c88ff1d9d6f0d6291a35654fd7cbb8
c0a242346c <c0a242346c@edu.teu.ac.jp>
2025-04-15 15:57:15 +0900

    背景をつなぐ

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -19,6 +19,7 @@ def main():
             if event.type == pg.QUIT: return
         x = tmr
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [-x+1600, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
